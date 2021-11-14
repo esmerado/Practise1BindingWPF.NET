4b533f9680229d95ae18aa433703398955b2ae67
esmerado <esmerado.vela.javier@gmail.com>
2021-11-14 13:33:43 +0100

    .NetApp_v.1

---
+++ BindingPractiseTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingPractiseTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SmallBtn_Event(object sender, RoutedEventArgs e)
+        {
+            TextSlider.Value = 15;
+        }
+
+        private void MeidumBtn_Event(object sender, RoutedEventArgs e)
+        {
+            TextSlider.Value = 20;
+        }
+
+        private void BigBtn_Event(object sender, RoutedEventArgs e)
+        {
+            TextSlider.Value = 25;
+        }
+
+    }
+}
